8e0245fda9ad3b09fd90574b6449a284c19a3202
brunoodon <adm@brunoodon.com.br>
2023-03-28 00:17:58 -0300

    new file:   automation/search_hash.sh

---
+++ automation/search_hash.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+api_key='eGghpexlvMdaMm7O1fnuP6pq7HinefSEPzDbAhKi' 
+misp_url='https://192.168.1.50/attributes/restSearch'
+curl -k -H 'Accept: application/json' -H 'Content-type: application/json' -H 'Authorization: '$api_key'' -XGET $misp_url/returnFormat:text/type:md5||sha1||sha256||sha512
